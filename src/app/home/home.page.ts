ebf167dbf66ab395f55e7328f5f50329acbdd4bb
duocpv <duocpv@consultindochina.com>
2021-04-06 16:50:42 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+  scanActive= true;
+  scanResult= null;
+  @ViewChild('video',{static: false}) video: ElementRef
+  videoElement: any;
 
-  constructor() {}
+
+constructor(private qrScanner: QRScanner) { }
+
+qrScan() {
+  this.qrScanner.prepare()
+  .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+       // camera permission was granted
+
+
+       // start scanning
+       let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+         console.log('Scanned something', text);
+         this.scanResult = text;
+
+         this.qrScanner.hide(); // hide camera preview
+         scanSub.unsubscribe(); // stop scanning
+       });
+
+     } else if (status.denied) {
+       // camera permission was permanently denied
+       // you must use QRScanner.openSettings() method to guide the user to the settings page
+       // then they can grant the permission from there
+     } else {
+       // permission was denied, but not permanently. You can ask for permission again at a later time.
+     }
+  })
+  .catch((e: any) => console.log('Error is', e));
+}
+// Optionally request the permission early
+
 
 }
+
+
+
